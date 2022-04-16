baaea054e6d9f4cac4903ed778f8ed0d621f3dd3
weonalazarus <89036132+weonalazarus@users.noreply.github.com>
2022-04-16 10:13:46 +0530

    Create A7 Prims.cpp

---
+++ A7 Prims.cpp
@@ -0,0 +1,78 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+#define INF 9999999
+
+// number of vertices in grapj
+#define V 5
+
+// create a 2d array of size 5x5
+//for adjacency matrix to represent graph
+
+int G[V][V] = {
+  {0, 4, 0, 5, 2},
+  {4, 0, 1, 3, 0},
+  {0, 1, 0, 8, 0},
+  {5, 3, 8, 0, 2},
+  {2, 0, 0, 2, 0}};
+
+int main() {
+  int no_edge;  // number of edge
+
+  // create a array to track selected vertex
+  // selected will become true otherwise false
+  int selected[V];
+
+  // set selected false initially
+  memset(selected, false, sizeof(selected));
+
+  // set number of edge to 0
+  no_edge = 0;
+
+  // the number of egde in minimum spanning tree will be
+  // always less than (V -1), where V is number of vertices in
+  //graph
+
+  // choose 0th vertex and make it true
+  selected[0] = true;
+
+  int x;  //  row number
+  int y;  //  col number
+
+  // print for edge and weight
+  cout << "Edge"
+     << " : "
+     << "Weight";
+  cout << endl;
+  while (no_edge < V - 1) {
+    //For every vertex in the set S, find the all adjacent vertices
+    // , calculate the distance from the vertex selected at step 1.
+    // if the vertex is already in the set S, discard it otherwise
+    //choose another vertex nearest to selected vertex  at step 1.
+
+    int min = INF;
+    x = 0;
+    y = 0;
+
+    for (int i = 0; i < V; i++) {    //traverse along to row to pick the vertex
+      if (selected[i]) {             // after picking the vertex check if it is inside the array
+        for (int j = 0; j < V; j++) {          // now traverse along coloms
+          if (!selected[j] && G[i][j]) {  // not in selected and there is an edge
+            if (min > G[i][j]) {
+              min = G[i][j];
+              x = i;
+              y = j;
+            }
+          }
+        }
+      }
+    }
+    cout << x << " - " << y << " :  " << G[x][y];
+    cout << endl;
+    selected[y] = true;
+    no_edge++;
+  }
+
+  return 0;
+}
